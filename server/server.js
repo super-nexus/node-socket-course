9ef5d49c309fb80cf4263ae6c2b193c176f5b4e1
super-nexus <androidxl.news.xl@gmail.com>
2018-09-05 21:46:35 +0200

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const express = require('express')
+
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
+
+var app = express();
+
+app.use(express.static(publicPath));
+
+app.get('/', (req, res) => {
+  res.render('index.html');
+})
+
+
+app.listen(port, () => {
+  console.log('Server started at port:' + port);
+})
